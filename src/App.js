f47b0f314a621a61d95858ea67018d7e42f191f8
DESKTOP-17AVGLM\ramac <ramachandrajune25@gmail.com>
2023-09-08 21:16:43 +0530

    using redux toolkit, tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment } from "./redux/appSlice";
 function App() {
+  const dispatch=useDispatch();
+  const count=useSelector((store)=>store.count);
+  
+  const handleInc=()=>{
+    dispatch(increment());
+  }
+
+  const handleDec=()=>{
+    dispatch(decrement());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" justify-center h-20 align-middle grid grid-flow-row p-20">
+      <div className="text-3xl text-center mb-5">{count}</div>
+      <div className="flex">
+        <button className="p-3 border border-gray-500 rounded-lg mr-10" onClick={handleInc}>
+          Increment
+        </button>
+        <button className="p-3 border border-gray-500 rounded-lg" onClick={handleDec}>
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
